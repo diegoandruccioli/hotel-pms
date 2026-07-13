32e44d1570ca413e5daee962590613eebae05c44
diegoandruccioli <diego.andruccioli@studio.unibo.it>
2026-07-13 22:12:55 +0200

    fix(test): suppress JDT null-type-safety warnings in FatturaPAServiceImplTest

--- billing-service/src/test/java/com/hotelpms/billing/service/impl/FatturaPAServiceImplTest.java
+++ billing-service/src/test/java/com/hotelpms/billing/service/impl/FatturaPAServiceImplTest.java
@@ -35,6 +35,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
+@SuppressWarnings("null")
 @ExtendWith(MockitoExtension.class)
 class FatturaPAServiceImplTest {
 
